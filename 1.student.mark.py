c11fa65a9b0de7751e81737a3cade3a64df5a501
NoodleGodz <baybobi4@gmail.com>
2022-03-14 23:20:05 +0700

    Add Switch Case to 1.student.mark.py
    Add Switch Case to 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -8,16 +8,23 @@ def Studentpinput():
     for i in range(na):
         print(f"\nStudent {i+1}:")
         id=input("Enter ID: ")
+        if i!=0:
+            while id in list(list(zip(*studentlist))[0]) or id=="":
+                id=input("Enter new ID: ")
         name=input("Enter Name: ")
-        DoB=input("Enter DoB: ")
+        DoB=input("Enter DoB (dd-mm-yyyy) : ")
         a=[id,name,DoB]
         studentlist.append(a)
 
 def Coursepinput():
     na=int(input("\nEnter number of Courses: "))
     for i in range(na):
+
         print(f"\nCourse {i+1}:")
         id=input("Enter ID: ")
+        if i!=0:
+            while id in list(list(zip(*courselist))[0]) or id=="":
+                id=input("Enter new ID: ")
         name=input("Enter Name: ")
         a=[id,name]
         courselist.append(a)
@@ -29,17 +36,22 @@ def Coursepinput():
 
 def Markpinput(a):
     for i in courselist:
-        print(a, " vs ",i[0])
+        ##print(i[0]," = ",a)
         if i[0]==a:
             d=[]
             na=int(input(f"Enter number of Student in Courses {a} : "))
             for i in range(na):   
                 id=input("Enter ID of Student: ")
+                while id not in list(list(zip(*studentlist))[0]):
+                    id=input("Enter new ID: ")
                 mark=float(input("Enter Mark: "))
                 v=[id,mark]
                 d.append(v)  
             marklist.append([a,d])    
-        break    
+            return  
+    
+    print("Course ",a," marksheet not exist")
+
  
 
 
@@ -56,20 +68,50 @@ def listCourses():
 def listMarks(a):
     for i in marklist:
         if i[0]==a:
-            print(f"\nList of Marks in Course {a} : ")
+            print(f"\nCourse {a} Marksheet : ")
             for j in i[1]:
                 print(j[0],"-",j[1])
+            return
+    print(a,"not exist")
+            
 
 
 
-Studentpinput() 
-Coursepinput()
-
-Markpinput(courselist[0][0])
-
-listStudents()
-listCourses()
-listMarks(courselist[0][0])
 
 
+def get_case():
+    print("\n---------------------------")
+    print("\n1) Add students")
+    print("2) Add courses")
+    print("3) Add marks for a course")
+    print("4) Show all students")
+    print("5) Show all courses")
+    print("6) Show all marks in a course")
+    print("7) ESC")
+    num = int(input("Choose (1-7) : "))
+    print("---------------------------\n")
+    return num
+
+
+def Switch(d):
+    if d == 1:
+        Studentpinput()
+    elif d == 2:
+        Coursepinput()
+    elif d == 3:
+        Markpinput(input("Enter Course ID : "))
+    elif d == 4:
+        listStudents()
+    elif d == 5:
+        listCourses()
+    elif d == 6:
+        listMarks(input("Enter Course ID : "))
+    elif d == 7:
+        exit()   
+    else:
+        print("Error")     
+
+while True:
+    Switch(get_case())
+    
 
